30f9b4d21f6b9b8e4309bbfffdef6877534aa1c0
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-06-28 14:41:25 -0500

    Se retira libreria que no se usa

--- Clase9/src/app/client/login/LoginComponent.java
+++ Clase9/src/app/client/login/LoginComponent.java
@@ -8,7 +8,6 @@ import javax.swing.JOptionPane;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
