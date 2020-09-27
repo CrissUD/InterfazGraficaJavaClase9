95bef7ea4d17bd09bc8a7268e1231bf78fdc188c
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-09-27 09:01:29 -0500

    Cambios en el codigo para identacion y actualizacion

--- Clase9/src/app/client/components/navegacionUsuario/NavegacionUsuarioComponent.java
+++ Clase9/src/app/client/components/navegacionUsuario/NavegacionUsuarioComponent.java
@@ -3,52 +3,41 @@ package app.client.components.navegacionUsuario;
 import app.client.vistaPrincipal.VistaPrincipalComponent;
 import app.services.RecursosService;
 
-import javax.swing.JButton;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.MouseAdapter;
 
-public class NavegacionUsuarioComponent implements ActionListener, MouseListener {
+import javax.swing.JButton;
 
-    private NavegacionUsuarioTemplate navegacionUsuarioTemplate;
-    private VistaPrincipalComponent vistaPrincipalComponent;
+public class NavegacionUsuarioComponent extends MouseAdapter implements ActionListener {
+  private NavegacionUsuarioTemplate navegacionUsuarioTemplate;
+  private VistaPrincipalComponent vistaPrincipalComponent;
 
-    public NavegacionUsuarioComponent(VistaPrincipalComponent vistaPrincipalComponent) {
-        this.vistaPrincipalComponent = vistaPrincipalComponent;
-        this.vistaPrincipalComponent.getClass();
-        this.navegacionUsuarioTemplate = new NavegacionUsuarioTemplate(this);
-    }
+  public NavegacionUsuarioComponent(VistaPrincipalComponent vistaPrincipalComponent) {
+    this.vistaPrincipalComponent = vistaPrincipalComponent;
+    this.navegacionUsuarioTemplate = new NavegacionUsuarioTemplate(this);
+  }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        this.vistaPrincipalComponent.mostrarComponente(e.getActionCommand().trim());
-    }
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    this.vistaPrincipalComponent.mostrarComponente(e.getActionCommand().trim());
+  }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {}
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    JButton boton = ((JButton) e.getSource());
+    boton.setContentAreaFilled(true);
+    boton.setBackground(RecursosService.getService().getColorAzulOscuro());
+  }
 
-    @Override
-    public void mousePressed(MouseEvent e) {}
+  @Override
+  public void mouseExited(MouseEvent e) {
+    JButton boton = ((JButton) e.getSource());
+    boton.setContentAreaFilled(false);
+  }
 
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        JButton boton = ((JButton) e.getSource());
-        boton.setContentAreaFilled(true);
-        boton.setBackground(RecursosService.getService().getColorAzulOscuro()); 
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        JButton boton = ((JButton) e.getSource());
-        boton.setContentAreaFilled(false);
-    }
-    
-    public NavegacionUsuarioTemplate getNavegacionUsuarioTemplate() {
-        return this.navegacionUsuarioTemplate;
-    }
+  public NavegacionUsuarioTemplate getNavegacionUsuarioTemplate() {
+    return this.navegacionUsuarioTemplate;
+  }
 }
--- Clase9/src/app/services/RecursosService.java
+++ Clase9/src/app/services/RecursosService.java
@@ -3,107 +3,111 @@ package app.services;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.border.Border;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 
 public class RecursosService {
+  private Color colorAzul, colorAzulOscuro, colorMorado, colorGrisOscuro, colorGrisClaro;
+  private Font fontTPrincipal, fontTitulo, fontSubtitulo;
+  private Font fontMediana, fontLigera;
+  private Cursor cMano;
+  private Border bInferiorAzul, bInferiorGris, bGris;
+  private ImageIcon iCerrar, iMinimizar;
 
-    private Color colorAzul, colorAzulOscuro, colorMorado, colorGrisOscuro, colorGrisClaro;
-    private Font fontTPrincipal, fontTitulo, fontSubtitulo;
-    private Font fontBotones, fontPequeña;
-    private Cursor cMano;
-    private Border borderInferiorAzul, borderInferiorGris, borderGris;
-    private ImageIcon iCerrar, iMinimizar;
+  static private RecursosService servicio;
 
-    static private RecursosService servicio;
+  private RecursosService() {
+    generarFuentes();
 
-    private RecursosService(){
-        colorMorado = new Color(151, 0, 158);
-        colorAzul = new Color(60, 78, 120);
-        colorAzulOscuro = new Color(30, 48, 90);
-        colorGrisOscuro = new Color(80, 80, 80);
-        colorGrisClaro = new Color(249, 249, 249);
-        fontTPrincipal = new Font("Rockwell Extra Bold", Font.PLAIN, 20);
-        fontTitulo = new Font("LuzSans-Book", Font.BOLD, 17);
-        fontSubtitulo = new Font("Forte", Font.PLAIN, 13);
-        fontBotones = new Font("LuzSans-Book", Font.PLAIN, 15);
-        fontPequeña = new Font("LuzSans-Book", Font.PLAIN, 12);
-        cMano = new Cursor(Cursor.HAND_CURSOR);
-        borderInferiorAzul = BorderFactory.createMatteBorder(0, 0, 2, 0, colorAzul);
-        borderInferiorGris = BorderFactory.createMatteBorder(0, 0, 1, 0, Color.LIGHT_GRAY);
-        borderGris = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2, true);
-        iCerrar = new ImageIcon("Clase9/resources/img/cerrar.png");
-        iMinimizar = new ImageIcon("Clase9/resources/img/minimizar.png");
-    }
-    
-    public Color getColorMorado(){
-        return colorMorado;
-    }
+    this.crearColores();
+    this.crearFuentes();
+    this.crearCursores();
+    this.crearBordes();
+    this.crearImagenes();
+  }
 
-    public Color getColorAzul(){
-        return colorAzul;
-    }
+  private void crearColores() {
+    colorAzul = new Color(60, 78, 120);
+    colorAzulOscuro = new Color(30, 48, 90);
+    colorMorado = new Color(151, 0, 158);
+    colorGrisOscuro = new Color(80, 80, 80);
+    colorGrisClaro = new Color(249, 246, 249);
+  }
 
-    public Color getColorAzulOscuro(){
-        return colorAzulOscuro;
-    }
+  private void crearFuentes() {
+    fontTPrincipal = new Font("Rockwell Extra Bold", Font.PLAIN, 20);
+    fontTitulo = new Font("Calibri (Cuerpo)", Font.BOLD, 17);
+    fontSubtitulo = new Font("Forte", Font.PLAIN, 13);
+    fontMediana = new Font("LuzSans-Book", Font.PLAIN, 15);
+    fontLigera = new Font("LuzSans-Book", Font.PLAIN, 12);
+  }
 
-    public Color getColorGrisOscuro(){
-        return colorGrisOscuro;
-    }
+  private void crearCursores() { cMano = new Cursor(Cursor.HAND_CURSOR); }
 
-    public Color getColorGrisClaro(){
-        return colorGrisClaro;
-    }
+  private void crearBordes() {
+    bInferiorAzul = BorderFactory.createMatteBorder(0, 0, 2, 0, colorAzul);
+    bInferiorGris = BorderFactory.createMatteBorder(0, 0, 1, 0, Color.LIGHT_GRAY);
+    bGris = BorderFactory.createLineBorder(Color.LIGHT_GRAY, 2, true);
+  }
 
-    public Font getFontTPrincipal(){
-        return fontTPrincipal;
-    }
+  private void crearImagenes() {
+    iCerrar = new ImageIcon("Clase9/resources/images/cerrar.png");
+    iMinimizar = new ImageIcon("Clase9/resources/images/minimizar.png");
+  }
 
-    public Font getFontTitulo(){
-        return fontTitulo;
+  private void generarFuentes() {
+    try {
+      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+      ge.registerFont( Font.createFont(
+          Font.TRUETYPE_FONT,
+          new File("Clase9/resources/fonts/LUZRO.ttf")
+      ));
+    } catch (IOException | FontFormatException e) {
+      System.out.println(e);
     }
-    
-    public Font getFontSubtitulo(){
-        return fontSubtitulo;
-    }
-    
-    public Font getFontBotones(){
-        return fontBotones;
-    }
+  }
 
-    public Font getFontPequeña(){
-        return fontPequeña;
-    }
+  public Color getColorAzul() { return colorAzul; }
 
-    public Cursor getCMano(){
-        return cMano;
-    }
+  public Color getColorAzulOscuro(){ return colorAzulOscuro; }
 
-    public Border getBorderInferiorAzul(){
-        return borderInferiorAzul;
-    }
+  public Border getBInferiorGris(){ return bInferiorGris; }
+  
+  public Color getColorGrisOscuro() { return colorGrisOscuro; }
+  
+  public Color getColorMorado(){ return colorMorado; }
+  
+  public Color getColorGrisClaro(){ return colorGrisClaro; }
 
-    public Border getBorderInferiorGris(){
-        return borderInferiorGris;
-    }
+  public Font getFontTPrincipal() { return fontTPrincipal; }
 
-    public Border getBorderGris(){
-        return borderGris;
-    }
+  public Font getFontTitulo() { return fontTitulo; }
 
-    public ImageIcon getICerrar(){
-        return iCerrar;
-    }
+  public Font getFontSubtitulo() { return fontSubtitulo; }
 
-    public ImageIcon getIMinimizar(){
-        return iMinimizar;
-    }
+  public Font getFontMediana() { return fontMediana; }
 
-    public static RecursosService getService(){
-        if(servicio == null)
-            servicio = new RecursosService();
-        return servicio;
-    }
+  public Font getFontLigera(){ return fontLigera; }
+
+  public Cursor getCMano() { return cMano; }
+
+  public Border getBInferiorAzul() { return bInferiorAzul; }
+
+  public Border getBGris(){ return bGris; }
+
+  public ImageIcon getICerrar() { return iCerrar; }
+
+  public ImageIcon getIMinimizar() { return iMinimizar; }
+
+  public static RecursosService getService() {
+    if (servicio == null) servicio = new RecursosService();
+    return servicio;
+  }
 }
